cfd310cc530170c637aa31105344194698292395
karlo <wenshan747@126.com>
2019-05-08 21:37:59 +0800

    11

--- provider/src/main/java/com/karlo/provider/service/LoginCheckImpl.java
+++ provider/src/main/java/com/karlo/provider/service/LoginCheckImpl.java
@@ -2,7 +2,12 @@ package com.karlo.provider.service;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.karlo.providerAPI.LoginCheck;
+import lombok.Data;
+import org.junit.jupiter.api.Test;
 import org.springframework.stereotype.Component;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Service(interfaceClass = LoginCheck.class)
 @Component
@@ -17,4 +22,35 @@ public class LoginCheckImpl implements LoginCheck {
         return false;
 
     }
+    @Test
+    public void test(){
+        User user = new User();
+        user.setName(null);
+        t(user);
+    }
+
+
+    public void t(@Valid User user){
+        System.out.println(user.getName());
+        System.out.println(user.getCardId());
+    }
+
+
+
 }
+
+
+@Data
+class User {
+
+    @NotNull
+    private String name;
+    @NotNull
+    private int id;
+    @NotNull
+    private double cardId;
+
+}
+
+
+
